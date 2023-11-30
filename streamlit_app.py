e6cc26ba9b601cfcf05b46925ce7f7eb2f636cdd
AdrienMarquer22 <amarquer@eulidia.com>
2023-11-30 11:20:22 +0100

    tt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry')
+streamlit.text('Omega 3 & blueberry')
+streamlit.text('Omega 3 & blueberry')
